645733a8621a205ba938213bc4efa1c8b2ab959c
Anders Newgard <anne9707@colorado.edu>
2023-08-06 20:42:44 -0400

    added initial prompts, alerts, and confirmation messages containing questions about which characters the user wants to include in their password.

--- script.js
+++ script.js
@@ -103,11 +103,52 @@ var uppercase = [
 "Z"
 ]
 
-
-
 // Write password to the #password input
 function generatePassword() {
+  var password = "";
+  var passwordChar = "";
+  var passwordLengthUser = prompt("How many characters would you like your password to be? Must be between 8 and 128 characters.");
+  passwordLengthUser = parseInt(passwordLengthUser);
+
+  if (passwordLengthUser < 8) {
+      alert("Password must have more than 7 characters!")
+  }
+
+  if (passwordLengthUser > 128) {
+      alert("Password must have less than 129 characters!")
+  }
+
+  var lowercaseCharChoice = confirm("Would you like to add lowercase characters to the password?");
+
+  if (lowercaseCharChoice) {
+      passwordChar += lowercase;
+  }
   
+  var uppercaseCharChoice = confirm("How about some uppercase letters?");
+
+  if (uppercaseCharChoice) {
+      passwordChar += uppercase;
+  }
+
+  var specialcharactersCharChoice = confirm("Next, would you like to see some special symbols in there?");
+
+  if (specialcharactersCharChoice) {
+      passwordChar += specialchar;
+  }
+
+  var numericalCharChoice = confirm("And finally, how about numbers?");
+
+  if (numericalCharChoice) {
+      passwordChar += num;
+  }
+
+  for (var i = 0; i < passwordLengthUser; i++) {
+      password = passwordChar[Math.floor(Math.random() * passwordChar.length)]
+  }
+
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 
 
 function writePassword() {
