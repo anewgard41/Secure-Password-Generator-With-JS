e23988603614df44b4755a0eaa0d5aad3f3831bb
Anders Newgard <anne9707@colorado.edu>
2023-08-08 23:51:49 -0400

    fixed functions positioning. the function writepassword was accidentally placed inside the scope of generatepassword originally.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Secure-Password-Generator-With-JS
+
--- script.js
+++ script.js
@@ -2,161 +2,172 @@
 var generateBtn = document.querySelector("#generate");
 
 var specialchar = [
-"!",
-"@",
-"#",
-"$",
-"%",
-"^",
-"&",
-"*",
-"(",
-")",
-"_",
-"+",
-"`",
-"-",
-"=",
-"[",
-"]",
-"{",
-"}",
-"|",
-"<",
-",",
-".",
-">",
-"?",
-"/",
-";",
-"'"  
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "+",
+    "`",
+    "-",
+    "=",
+    "[",
+    "]",
+    "{",
+    "}",
+    "|",
+    "<",
+    ",",
+    ".",
+    ">",
+    "?",
+    "/",
+    ";",
+    "'"
 ]
 
-var num = [
-"0",
-"1",
-"2",
-"3",
-"4",
-"5",
-"6",
-"7",
-"8",
-"9"
+var numbers = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9"
 ]
 
 var lowercase = [
-"a",
-"b",
-"c",
-"d",
-"e",
-"f",
-"g",
-"h",
-"i",
-"j",
-"k",
-"l",
-"m",
-"n",
-"o",
-"p",
-"q",
-"r",
-"s",
-"t",
-"u",
-"v",
-"w",
-"x",
-"y",
-"z"
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
 ]
 
 var uppercase = [
-"A",
-"B",
-"C",
-"D",
-"E",
-"F",
-"G",
-"H",
-"I",
-"J",
-"K",
-"L",
-"M",
-"N",
-"O",
-"P",
-"Q",
-"R",
-"S",
-"T",
-"U",
-"V",
-"W",
-"X",
-"Y",
-"Z"
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
 ]
 
 // Write password to the #password input
 function generatePassword() {
- 
-  var password = "";
-  var passwordChar = "";
-  var passwordLengthUser = prompt("How many characters would you like your password to be? Must be between 8 and 128 characters.");
-  passwordLengthUser = parseInt(passwordLengthUser);
+    console.log('running');
+    var password = "";
+    var passwordChar = "";
+    var passwordLengthUser = prompt("How many characters would you like your password to be? Must be between 8 and 128 characters.");
+    passwordLengthUser = parseInt(passwordLengthUser);
 
-  if (passwordLengthUser < 8) {
-      alert("Password must have more than 7 characters!")
-      return "";
-  }
+    // if user presser cancel, ends the function.
+    if (!passwordLengthUser) {
+        return;
+    }
 
-  if (passwordLengthUser > 128) {
-      alert("Password must have less than 129 characters!")
-      return "";
-  }
+    // if user enters a value under 8 or above 128, the function ends. 
+    if (passwordLengthUser < 8) {
+        alert("Password must have more than 7 characters!")
+        return "";
+    };
 
-  var lowercaseCharChoice = confirm("Would you like to add lowercase characters to the password?");
+    if (passwordLengthUser > 128) {
+        alert("Password must have less than 129 characters!")
+        return "";
+    };
 
-  if (lowercaseCharChoice) {
-      passwordChar += lowercase;
-  }
-  
-  var uppercaseCharChoice = confirm("How about some uppercase letters?");
+    var lowercaseCharChoice = confirm("Would you like to add lowercase characters to the password?");
 
-  if (uppercaseCharChoice) {
-      passwordChar += uppercase;
-  }
+    if (lowercaseCharChoice) {
+        passwordChar += lowercase;
+    };
 
-  var specialcharactersCharChoice = confirm("Next, would you like to see some special symbols in there?");
+    var uppercaseCharChoice = confirm("How about some uppercase letters?");
 
-  if (specialcharactersCharChoice) {
-      passwordChar += specialchar;
-  }
+    if (uppercaseCharChoice) {
+        passwordChar += uppercase;
+    };
 
-  var numericalCharChoice = confirm("And finally, how about numbers?");
+    var specialcharactersCharChoice = confirm("Next, would you like to see some special symbols in there?");
 
-  if (numericalCharChoice) {
-      passwordChar += num;
-  }
+    if (specialcharactersCharChoice) {
+        passwordChar += specialchar;
+    };
 
-  for (var i = 0; i < passwordLengthUser; i++) {
-      password = passwordChar[Math.floor(Math.random() * passwordChar.length)]
-  }
+    var numericalCharChoice = confirm("And finally, how about numbers?");
 
+    if (numericalCharChoice) {
+        passwordChar += numbers;
+    };
+
+    
+
+    for (var i = 0; i < passwordLengthUser; i++) {
+        password += passwordChar[Math.floor(Math.random() * passwordChar.length)]
+    };
+
+
+
+    return password
+}
 
 function writePassword() {
-  var password = generatePassword();
-  var pwTextArea = document.getElementById("password");
-  pwTextArea.value = password;
+    var password = generatePassword();
+    var pwTextArea = document.querySelector("#password");
+    pwTextArea.value = password;
+
 }
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
-}
-
-
