a7c4cbb40106ecc6a57dfd1e3716635fdb78e2ed
Anders Newgard <ders@Anderss-MacBook-Air.local>
2023-08-09 18:18:02 -0400

    Added more comments regarding functionality in Javascript file.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// assigning character sets to variables. Strings to be specific. 
 var specialchar = "!@#$%^&*()_+`-=[]{}|<.>?/;,"
 var numbers = "0123456789"
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
@@ -8,10 +9,12 @@ var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 // Write password to the #password input
 function generatePassword() {
+// Created an empty string for password and passwordCharacter set to be run in writepassword function later. 
     console.log('running');
     var password = "";
     var passwordChar = "";
     var passwordLengthUser = prompt("How many characters would you like your password to be? Must be between 8 and 128 characters.");
+    // parseInt turns the answer from the user from a string to a numerical variable. 
     passwordLengthUser = parseInt(passwordLengthUser);
 
     // if user presser cancel, ends the function.
@@ -30,6 +33,9 @@ function generatePassword() {
         return "";
     };
 
+    // window.confirm used to get true or false answer from user for each of the four character sets. 
+
+    // CharChoice variables used for if statements. 
     var lowercaseCharChoice = confirm("Would you like to add lowercase characters to the password?");
 
     if (lowercaseCharChoice) {
@@ -54,6 +60,8 @@ function generatePassword() {
         passwordChar += numbers;
     };
 
+
+    // Used the function charAt so it would return a specific character at the random index from a string.
     for (var i = 0; i < passwordLengthUser; i++) {
         password += passwordChar.charAt(Math.floor(Math.random() * passwordChar.length))
     };
@@ -63,6 +71,8 @@ function generatePassword() {
 
 function writePassword() {
     var password = generatePassword();
+
+    // querySelector grabs location in DOM where the password will be input. 
     var pwTextArea = document.querySelector("#password");
     pwTextArea.value = password;
 
